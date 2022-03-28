3f67789c26048b4a7ad71c5518d66e209d80d168
曹凱嵐Jacky <kaij1941@gmail.com>
2022-03-28 23:27:48 +0800

    改寫第一版(有異常)

--- server.js
+++ server.js
@@ -1,117 +1,119 @@
-const { error } = require('console');
-const http = require('http');
-const { v4: uuidv4 } = require('uuid');
-const errorHandle=require('./errorHandle');
+const http=require('http');
+const { resolveSoa}=require('dns');
+const {error } =require('console');
+const { exit} =require('process');
+const {v4:uuidv4}=require('uuid');
+const errorHandle =require('./errorHandle');
 const todos =[];
 
-const requestListener = (req,res)=>{
-    const headers ={
+const requestlistener = (req,res)=>{
+    const header={
         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
         'Content-Type': 'application/json'
     }
-    console.log(req.url);
-    console.log(req.method);
-    let body =""
+    var urlRoute =req.url.split('/');
+    let body = "";
     req.on('data',chunk=>{
-        body += chunk;
+        body+=chunk;
     })
+    let successResult={
+        "status":"success",
+        "data":[]
+    }
+    if (urlRoute[1]=='todos') {
+        switch (req.method) {
+            case "GET":
+                SetDataSuccessRespond(res, header, successResult);
+                return;
+            case "POST":
+                req.on('end',()=>{
+                    try {
+                        const title =JSON.parse(body).title;
+                        if(title){
+                            const todo ={
+                                "id":uuid4(),
+                                "title":title
+                            }   
+                            todos.push(todo);
+                        }
+                        res.writeHead(200, header);
+                        successResult.data = todos;
+                        res.write(JSON.stringify(successResult));
+                        res.end();
+                        return;
+                    } catch (error) {
+                        errorHandle(res,400,"欄位未填寫正確");
+                        return;
+                    }
+                })
+                return;
+                break;
+            case "PATCH":
+                req.on('end',()=>{
+                    try {
+                        const title =JSON.parse(body).title;
+                        const id =urlRoute[2];
+                        const index = todos.findIndex(element=>element.id == id);
+                        if (title && index != -1 ){
+                            todos[index].title=title;
+                            SetDataSuccessRespond(res, header, successResult);
+                            return;
+                        } 
+                        errorHandle(res,400,"欄位未填寫正確 或 id不存在")
+                        return;
+                    } catch (error) {
+                        errorHandle(res,400,"欄位未填寫正確");
+                    }
+                    return;
+                })
+                break;
+            case "DELETE":
+                if(urlRoute[2]){
+                    res.on('end',()=>{
+                        const id =urlRoute[2];
+                        const index =todos.findIndex(element=>element.id==id);
+                        if (index !=-1) {
+                            todos.splice(index,1);
+                            SetDataSuccessRespond(res, header, successResult);
+                            return;
+                        }
+                        errorHandle(res,400,"id不存在");
+                        return;
+                    })                  
+                }
 
-    //首頁判定 且為GET
-    if (req.url=="/todos" && req.method=="GET") {
-        res.writeHead(200,headers);
-        res.write(JSON.stringify({
-            "status":"success",
-            "data":todos
+                todos.length=0;
+                SetDataSuccessRespond(res, header, successResult);
+                return;
+                break;
         }
-        ));
-        res.end();        
-    }else if(req.url =="/todos" && req.method == "POST"){        
-        req.on('end',()=>{
-            try {
-                const title =JSON.parse(body).title;
-                if (title !== undefined ) {
-                    const todo={
-                        "title":title,
-                        "id":uuidv4()
-                    }
-                    todos.push(todo);
-              
-                    res.writeHead(200,headers);
-                    res.write(JSON.stringify({
-                        "status":"success",
-                        "data":todos
-                    }
-                    ));
-                res.end();
-                }else{
-                    errorHandle(res);
-                }
-            } catch (error) {
-                errorHandle(res);
-            }           
-        })         
-    
-    
-    }else if(req.url =="/todos" && req.method == "DELETE"){
-        todos.length = 0;//清空todos
-        res.writeHead(200,headers);
-        res.write(JSON.stringify({
-            "status":"success",
-            "data":todos
-        }));
-        res.end();   
-    }else if(req.url.startsWith("/todos/") && req.method == "DELETE"){
-        const id = req.url.split('/').pop();
-        const index =todos.findIndex(element=>element.id == id);
-        console.log(index);
-        if (index !== -1) {
-            todos.splice(index,1);
-            res.writeHead(200,headers);
-            res.write(JSON.stringify({
-                "status":"success",
-                "data":todos
-            }));
-        }else{
-            errorHandle(res);
-        }
-        res.end();
-    }else if(req.url.startsWith("/todos/") && req.method == "PATCH"){
-            req.on('end',()=>{
-                try {
-                    const todo= JSON.parse(body).title;
-                    const id = req.url.split('/').pop();
-                    const index =todos.findIndex( element => element.id==id);
-                    if (todo !== undefined && index !== -1) {
-                        todos[index].title =todo;                       
-                        res.writeHead(200,headers);
-                        res.write(JSON.stringify({
-                            "status":"success",
-                            "data":todos
-                        }));
-                    }else{
-                        errorHandle(res);
-                    }
-                    res.end();
-                } catch (error) {
-                    errorHandle(res);
-                }
-            })
-    }else if(req.method == "OPTIONS"){
-        res.writeHead(200,headers);
-        res.end();
-    }else{
-        res.writeHead(404,headers);
-        res.write(JSON.stringify({
-            "status":"false",
-            "message":"無此網站路由"
-        }));
-        res.end();
     }
 
+    if (req.method =='Option'){
+        res.writeHead(200,header);
+        res.end();
+        return;   
+    }
+
+    errorHandle(res,404,"無此網路路由");
+    /**
+     * 錯誤處理
+     * @param {*} res 
+     * @param {*} httpCode 
+     * @param {*} message 
+     */
 
 }
 
-const server =http.createServer(requestListener);
-server.listen(process.env.PORT || 3005);
+const server =http.createServer(requestlistener);
+server.listen(3005);
+
+function SetDataSuccessRespond(res, header, successResult) {
+    res.writeHead(200, header);
+    successResult.data = todos;
+    res.write(JSON.stringify(successResult));
+    res.end();
+}
+
